96c51cb12f3de9457beae97614116199664e4789
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-30 18:10:44 +0530

    Modified IGenericRepository's GetAsync() signature

--- LeaveManagement.Web/Contracts/IGenericRepository.cs
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -3,7 +3,7 @@
     public interface IGenericRepository<T> where T : class
     {
         Task<List<T>> GetAllAsync();
-        Task<T> GetAsync(int id);
+        Task<T?> GetAsync(int? id);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id);
