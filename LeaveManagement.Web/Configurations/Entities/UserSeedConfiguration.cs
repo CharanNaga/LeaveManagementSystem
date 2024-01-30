d050f9d143eba4d61387d271a6bd7c4344c74a32
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-30 21:34:59 +0530

    Created a user seed config file for seeding data IdentityUser<T> with admin & user types

---
+++ LeaveManagement.Web/Configurations/Entities/UserSeedConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using LeaveManagement.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeaveManagement.Web.Configurations.Entities
+{
+    public class UserSeedConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            var hashPassword = new PasswordHasher<Employee>();
+            builder.HasData(
+                new Employee //admin user
+                {
+                    Id = "F5BC4ECE-E0D9-46B4-9763-FDD3D6745B58",
+                    Email = "admin@localhost.com",
+                    NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                    FirstName = "System",
+                    LastName = "Admin",
+                    PasswordHash = hashPassword.HashPassword(null, "Admin@123")
+                },
+                new Employee //normal user
+                {
+                    Id = "FBCE25C0-9092-4A1D-86F0-3F0D73D03768",
+                    Email = "user@localhost.com",
+                    NormalizedEmail = "USER@LOCALHOST.COM",
+                    FirstName = "System",
+                    LastName = "User",
+                    PasswordHash = hashPassword.HashPassword(null, "User@123")
+                }
+            );
+        }
+    }
+}
