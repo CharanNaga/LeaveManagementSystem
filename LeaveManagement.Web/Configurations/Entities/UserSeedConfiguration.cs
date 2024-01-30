e95f15b8a01d90bf78e635ae4217ece34ce79f8e
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-30 21:49:01 +0530

    Modified UserSeedCOnfiguration to include few more properties while seeding which are used during login

--- LeaveManagement.Web/Configurations/Entities/UserSeedConfiguration.cs
+++ LeaveManagement.Web/Configurations/Entities/UserSeedConfiguration.cs
@@ -9,25 +9,31 @@ namespace LeaveManagement.Web.Configurations.Entities
     {
         public void Configure(EntityTypeBuilder<Employee> builder)
         {
-            var hashPassword = new PasswordHasher<Employee>();
+            var hashPassword = new PasswordHasher<Employee>(); //aspnet users table
             builder.HasData(
                 new Employee //admin user
                 {
                     Id = "F5BC4ECE-E0D9-46B4-9763-FDD3D6745B58",
                     Email = "admin@localhost.com",
                     NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                    UserName = "admin@localhost.com",
+                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
                     FirstName = "System",
                     LastName = "Admin",
-                    PasswordHash = hashPassword.HashPassword(null, "Admin@123")
+                    PasswordHash = hashPassword.HashPassword(null, "Admin@123"),
+                    EmailConfirmed = true
                 },
                 new Employee //normal user
                 {
                     Id = "FBCE25C0-9092-4A1D-86F0-3F0D73D03768",
                     Email = "user@localhost.com",
                     NormalizedEmail = "USER@LOCALHOST.COM",
+                    UserName = "user@localhost.com",
+                    NormalizedUserName = "USER@LOCALHOST.COM",
                     FirstName = "System",
                     LastName = "User",
-                    PasswordHash = hashPassword.HashPassword(null, "User@123")
+                    PasswordHash = hashPassword.HashPassword(null, "User@123"),
+                    EmailConfirmed = true
                 }
             );
         }
