ae3a98813ecbcddb9c0bcac521112f5a67c7c157
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-31 21:28:47 +0530

    Modified LeaveAllocationViewModel by placing data annotations to the properties

--- LeaveManagement.Web/Models/LeaveAllocationViewModel.cs
+++ LeaveManagement.Web/Models/LeaveAllocationViewModel.cs
@@ -1,10 +1,21 @@
-﻿namespace LeaveManagement.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
 {
     public class LeaveAllocationViewModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Number Of Days")]
+        [Range(1,50,ErrorMessage = "Number Lies between 1 & 50")]
         public int NumberOfDays { get; set; }
+
+        [Required]
+        [Display(Name = "Allocation Period")]
         public int Period { get; set; }
-        public LeaveTypeViewModel LeaveType { get; set; }
+
+        public LeaveTypeViewModel? LeaveType { get; set; }
     }
 }
