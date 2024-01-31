9c2853c240fcff91cbe58ca382f9ff58b66870fa
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-31 19:11:47 +0530

    Created two new view models for handling Leave Allocations for an Employee

---
+++ LeaveManagement.Web/Models/LeaveAllocationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.Web.Models
+{
+    public class LeaveAllocationViewModel
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public int Period { get; set; }
+        public LeaveTypeViewModel LeaveType { get; set; }
+    }
+}
