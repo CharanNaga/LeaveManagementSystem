506e57aff8f79582ba6857ad6f945857bd97ae6b
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-02-01 13:03:08 +0530

    Added the LeaveRequest as a DbSet in ApplicationDbContext & done code-first migrations for reflecting in sql

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace LeaveManagement.Web.Data
         }
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
