d074c854fc8901c6ed3c902e9ca107bf3f400cbb
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-02-01 17:15:51 +0530

    Created two new view models to handle leave request entity

---
+++ LeaveManagement.Web/Models/LeaveRequestViewModel.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagement.Web.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveRequestViewModel : LeaveRequestCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Requested Date")]
+        public DateTime RequestedDate { get; set; }
+
+        [Display(Name = "Leave Type")]
+        public LeaveTypeViewModel LeaveType { get; set; }
+
+        public bool? IsApproved { get; set; }
+        public bool IsCancelled { get; set; }
+    }
+}
