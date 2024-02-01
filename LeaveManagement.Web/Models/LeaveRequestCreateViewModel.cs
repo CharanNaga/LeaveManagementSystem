1c9d90e9c20556df7a2055cca1ca4341d799e1bd
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-02-01 14:21:49 +0530

    Created a view model for the LeaveRequest data model

---
+++ LeaveManagement.Web/Models/LeaveRequestCreateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveRequestCreateViewModel : IValidatableObject
+    {
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime? StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime? EndDate { get; set; }
+
+        [Required]
+        public int LeaveTypeId { get; set; }
+
+        public SelectList? LeaveTypes { get; set; }
+
+        [Display(Name = "Request Comments")]
+        public string? RequestComments { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(StartDate > EndDate)
+            {
+                yield return new ValidationResult(
+                    "Start Date must be earlier than End Date", new[] {nameof(StartDate),nameof(EndDate)}
+                    );
+            }
+
+            if(RequestComments?.Length > 250)
+            {
+                yield return new ValidationResult(
+                    "Request Comments are too long", new[] { nameof(RequestComments) }
+                    );
+            }
+        }
+    }
+}
