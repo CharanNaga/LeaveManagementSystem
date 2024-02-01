2ed8a732d952b44a4e7494085146d3fa2aa3fb0f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-02-01 14:23:58 +0530

    Created a new method CreateLeaveRequest() in contract & implemented the same in the repository

--- LeaveManagement.Web/Repositories/LeaveRequestRepository.cs
+++ LeaveManagement.Web/Repositories/LeaveRequestRepository.cs
@@ -1,12 +1,36 @@
-﻿using LeaveManagement.Web.Contracts;
+﻿using AutoMapper;
+using LeaveManagement.Web.Contracts;
 using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace LeaveManagement.Web.Repositories
 {
     public class LeaveRequestRepository : GenericRepository<LeaveRequest>, ILeaveRequestRepository
     {
-        public LeaveRequestRepository(ApplicationDbContext db) : base(db)
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly UserManager<Employee> _userManager;
+
+        public LeaveRequestRepository(
+            ApplicationDbContext db, IMapper mapper,
+            IHttpContextAccessor contextAccessor,
+            UserManager<Employee> userManager
+            ) : base(db)
         {
+            _mapper = mapper;
+            _contextAccessor = contextAccessor;
+            _userManager = userManager;
+        }
+
+        public async Task CreateLeaveRequest(LeaveRequestCreateViewModel leaveRequestCreateViewModel)
+        {
+            var user = await _userManager.GetUserAsync(_contextAccessor?.HttpContext?.User);
+            var leaveRequest = _mapper.Map<LeaveRequest>(leaveRequestCreateViewModel);
+            leaveRequest.RequestedDate = DateTime.Now;
+            leaveRequest.RequestingEmployeeId = user.Id;
+
+            await AddAsync(leaveRequest);
         }
     }
 }
